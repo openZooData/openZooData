6a28fb39da22cbc2d5ed77b7fb931b35bd2c0a9d
openZooData <thorsten@codelab.cafe>
2026-06-03 21:35:31 +0200

    add: deploy_server.sh

--- source/helpers/deploy_server.sh
+++ source/helpers/deploy_server.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ROOT_DIR=”$(cd “$(dirname “${BASH_SOURCE[0]}”)/../..” && pwd)”
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 
-cd “$ROOT_DIR”
+cd "$ROOT_DIR"
 
-echo “========================================”
-echo “Pull from git”
-echo “========================================”
+echo "========================================"
+echo "Pull from git"
+echo "========================================"
 
 git pull
 
 echo
-echo “========================================”
-echo “Install requirements”
-echo “========================================”
+echo "========================================"
+echo "Install requirements"
+echo "========================================"
 
 source ../venv/bin/activate
 pip install -r source/requirements.txt
 
 echo
-echo “========================================”
-echo “Stop server”
-echo “========================================”
+echo "========================================"
+echo "Stop server"
+echo "========================================"
 
-“$ROOT_DIR/source/helpers/stop_server.sh”
-
-echo
-echo “========================================”
-echo “Start server”
-echo “========================================”
-
-“$ROOT_DIR/source/helpers/start_server.sh”
+"$ROOT_DIR/source/helpers/stop_server.sh"
 
 echo
-echo “Deploy finished.”
+echo "========================================"
+echo "Start server"
+echo "========================================"
+
+"$ROOT_DIR/source/helpers/start_server.sh"
+
+echo
+echo "Deploy finished."
